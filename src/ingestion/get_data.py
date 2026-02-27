e1e6034d04d97d837c95ccf2820e9c1c006e6d45
Pedro Augusto de Melo Borges <pedroameloborges@gmail.com>
2026-02-27 20:36:32 -0300

    Create tests and validation layer

--- src/ingestion/get_data.py
+++ src/ingestion/get_data.py
@@ -1,9 +1,11 @@
 import json
+from pathlib import Path
 import logging
 
 
-def get_selfdev_members():
-    with open('data/raw/data.json') as json_file:
+def get_selfdev_members(file_path):
+    
+    with open(file_path, encoding= 'UTF-8') as json_file:
         data = json.load(json_file)
         return data['selfdev_members']
         
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,11 +1,18 @@
-from src.ingestion import get_data
+from ingestion import get_data
+from config.settings import DATA_RAW_DIR
 
-def test_extract_json_corret():
-    keys = ['name', 'discord_name', 'reason_to_code']
+def test_all_members_have_required_keys():
     
-    result = get_data.get_selfdev_members()[0]
+    members = get_data.get_selfdev_members( DATA_RAW_DIR / "data.json")
     
-    assert set(result.keys()) == set(keys)
+    print(members)
     
+    REQUIRED_KEYS = {'name', 'discord_name', 'reason_to_code'}
 
-
+    for member in members:
+        assert REQUIRED_KEYS.issubset(member.keys())
+    
+def test_exists_members():
+    members = get_data.get_selfdev_members(DATA_RAW_DIR / "data.json")
+    
+    assert True if len(members) != 0 else False
